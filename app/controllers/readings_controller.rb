4050893ef2fa802c487d3ff46413c4115ab3dc80
parthibeyond <parthi@beyondfinity.com>
2017-04-05 20:02:30 +0530

    Fix stylesheet issue

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,8 +1,8 @@
 class ReadingsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
-    @readings = current_user.readings
+    # @readings = current_user.readings
   end
 
   def create
